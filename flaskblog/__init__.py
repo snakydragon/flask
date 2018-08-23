922c8fd1f27f55aa742eafa0aa976f9e15f63128
snakydragon <grmldysinaga@gmail.com>
2018-08-23 15:48:51 +0700

    Add blueprints

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,13 +2,25 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from flaskblog.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'e129c5430db79a95b627e07688a2a692'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-from flaskblog import routes
+
+mail = Mail(app)
+
+
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import  main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
+
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, \
-    TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, \
     ValidationError
 from flaskblog.models import User
@@ -19,14 +18,12 @@ class RegistrationForm(FlaskForm):
                                                  EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    @staticmethod
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. '
                                   'Please choose a different one')
 
-    @staticmethod
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
@@ -51,24 +48,36 @@ class UpdateAccountForm(FlaskForm):
         ['jpg', 'png', 'jpeg'])])
     submit = SubmitField('Update')
 
-    @staticmethod
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('That username is taken. '
-                                      'Please choose a different one')
+                                      'Please choose a different one.')
 
-    @staticmethod
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. '
-                                      'Please choose a different one')
+                                      'Please choose a different one.')
 
 
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. '
+                                  'You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
+    submit = SubmitField('Reset Password')
