2c3da73d56076e7005e4e1c9770f31ee9f43ebea
snakydragon <grmldysinaga@gmail.com>
2018-08-26 20:07:25 +0700

    Add custom error pages and favicon

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Create a simple blog using Flask.
+**Create a simple blog using Flask.**
 
 Based on Flask Tutorial by Corey Schafer
 https://www.youtube.com/playlist?list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH
+
+**Additional extra features:**
+- Delete old profile picture file while updating to a new one.
+- Add favicon.
--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -19,8 +19,9 @@ mail = Mail(app)
 from flaskblog.users.routes import users
 from flaskblog.posts.routes import posts
 from flaskblog.main.routes import  main
+from flaskblog.errors.handlers import errors
 
 app.register_blueprint(users)
 app.register_blueprint(posts)
 app.register_blueprint(main)
-
+app.register_blueprint(errors)
